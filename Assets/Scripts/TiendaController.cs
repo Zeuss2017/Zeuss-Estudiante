f56f064b389c0e02bc050d7087f005719e35583f
Camilo Andres Moreno <camolave12@gmail.com>
2017-08-06 16:35:46 -0500

    Añadi: Menu de Actividad y Tienda Virtual.

---
+++ Assets/Scripts/TiendaController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TiendaController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Volver()
+    {
+
+    }
+
+    public void ComprarAvatar1()
+    {
+
+    }
+    public void ComprarAvatar2()
+    {
+
+    }
+    public void ComprarAvatar3()
+    {
+
+    }
+    public void ComprarAvatar4()
+    {
+
+    }
+    public void ComprarPista()
+    {
+
+    }
+
+}
